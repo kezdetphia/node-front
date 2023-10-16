c83d3bcd695148fce50731eee09cdb754d2ba28c
kezdetphia <fehermark88@gmail.com>
2023-10-16 00:07:00 -0700

    login register ui

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 
 const LoginPage = () => {
   const [buttonText, setButtonText] = useState("Login");
+  const [isLogin, setIsLogin] = useState(true);
 
-  const [firstName, setFirstName] = useState('')
-  
+  const [firstName, setFirstName] = useState("");
 
   const blankForm = {
     firstName: "",
@@ -14,23 +14,32 @@ const LoginPage = () => {
     password: "",
     passwordAgain: "",
   };
-  
+
   const [formData, setFormData] = useState({ blankForm });
 
-
-  const handleChange=(e)=>{
-    const {name, value} = e.target
+  const handleChange = (e) => {
+    const { name, value } = e.target;
     setFormData({
-      ...formData, [name]: value
-    })
-    console.log(name, value)
-  }
+      ...formData,
+      [name]: value,
+    });
+    console.log(name, value);
+  };
 
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-    console.log('Formdata Submitted:', formData)
-    setFormData(blankForm);
-  }
+  const handleSubmit = (e) => { 
+    e.preventDefault();
+    if (!isLogin){
+      console.log("Formdata Submitted:", formData);
+      setFormData(blankForm);
+      setButtonText('Login')
+      setIsLogin(!isLogin)
+    } else {
+      console.log('login username:', formData.firstName)
+      setFormData(blankForm);
+    }
+  };
+
+
 
   return (
     <div className="h-screen w-screen bg-pink-100 flex justify-center items-center">
@@ -45,7 +54,7 @@ const LoginPage = () => {
                 value={formData.firstName}
                 onChange={handleChange}
                 name="firstName"
-                ></input>
+              ></input>
               <input
                 type="text"
                 className="w-full"
@@ -53,7 +62,7 @@ const LoginPage = () => {
                 placeholder="Surname"
                 name="surName"
                 value={formData.surName}
-                ></input>
+              ></input>
             </div>
             <input
               type="email"
@@ -62,23 +71,43 @@ const LoginPage = () => {
               placeholder="Email addres"
               name="email"
               value={formData.email}
-              ></input>
+            ></input>
             <input
               value={formData.password}
               onChange={handleChange}
               type="password"
               name="password"
               placeholder="Password"
-              ></input>
-            <input
-              type="password"
-              onChange={handleChange}
-              name="passwordAgain"
-              placeholder="Password Again"
-              value={formData.passwordAgain}
             ></input>
-            <div>
-              <button type="submit">{buttonText}</button>
+
+            {!isLogin &&
+            <input
+            type="password"
+            onChange={handleChange}
+            name="passwordAgain"
+            placeholder="Password Again"
+            value={formData.passwordAgain}
+            ></input>
+          }
+            <div className="flex flex-col mt-5 gap-y-2">
+              <button
+                className=" text-white bg-pink-500 border border-black shadow-lg "
+                type="submit"
+              >
+                {buttonText}
+              </button>
+
+              {isLogin && (
+                <button
+                  type="submit"
+                  onClick={() => {
+                    setButtonText('Register');
+                    setIsLogin(!isLogin);
+                  }}
+                >
+                  Register
+                </button>
+              )}
             </div>
           </form>
         </div>
